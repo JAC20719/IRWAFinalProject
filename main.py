3b0d7edc931cc3e073ab7c6e123508dd41049de5
Justin Canedy <jcanedy07@gmail.com>
2022-05-11 14:53:39 -0400

    here we go

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ Created on Thu Apr 28 22:20:27 2022
 @author: jcane
 """
 
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import playergamelog
+from nba_api.stats.library.parameters import SeasonAll
+from nba_api.stats.static import teams 
+
+import pandas as pd 
+
+
+def main():
+    all_players = players.get_players()
+    all_teams = teams.get_teams()
+    
+
+if __name__ == "__main__":
+    main()
