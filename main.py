57e4ef620af83e4abe4e7b1a12240092ea2e5854
Justin Canedy <jcanedy07@gmail.com>
2022-05-11 15:53:03 -0400

    Set up

--- main.py
+++ main.py
@@ -6,17 +6,36 @@ Created on Thu Apr 28 22:20:27 2022
 """
 
 from nba_api.stats.static import players
-from nba_api.stats.endpoints import playergamelog
+from nba_api.stats.endpoints import playergamelog, cumestatsteam, teamgamelog, cumestatsteamgames
 from nba_api.stats.library.parameters import SeasonAll
 from nba_api.stats.static import teams 
 
 import pandas as pd 
 
+import sys
 
-def main():
+def game_predict(year,home_team,away_team,playoff):
+    winning_team = home_team
+    win_margin = 0
+    
+    game_ids = cumestatsteamgames.CumeStatsTeamGames(team_id=home_team,season=year).get_data_frames()[0].get("GAME_ID").tolist()
+    
+    home_team_stats = cumestatsteam.CumeStatsTeam(home_team, game_ids).get_data_frames()
+    print(home_team_stats[0].get("FG_PCT"))
+    
+    return winning_team, win_margin
+
+def main(argv):
     all_players = players.get_players()
     all_teams = teams.get_teams()
     
+    atl_id = all_teams[0]["id"]
+    
+    '''2021-22'''
+    
+    game_predict('2021-22', atl_id, 0, False)
+        
+    
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
