263a4b53d1f14cb3c43908495793e52124c9de82
isaacfrumkin <ifrumki1@jhu.edu>
2022-05-12 16:32:11 -0400

    away/home in log

--- data.py
+++ data.py
@@ -5,24 +5,32 @@ from nba_api.stats.endpoints import leaguegamelog
 
 def label(row):
     result = row.get("WL")
-    print(result)
+    # print(result)
     matchup = row.get("MATCHUP").split()
     mid = matchup[1]
+    first = matchup[0]
+    second = matchup[2]
     abr = row.get("TEAM_ABBREVIATION")
-    print(matchup)
-    print(abr)
+    # print(matchup)
+    # print(abr)
     status = ""
+    home = ""
+    away = ""
     if mid == "@":
+        home = second
+        away = first
         if abr == matchup[0]:
             status = "AWAY"
         else:
             status = "HOME"
     if mid == "vs.":
+        home = first
+        away = second
         if abr == matchup[2]:
             status = "AWAY"
         else:
             status = "HOME"
-    print(status)
+    # print(status)
     label = ""
     if result == "W":
         label = status
@@ -32,8 +40,8 @@ def label(row):
             label = "HOME"
         if status == "HOME":
             label = "AWAY"
-    print("LABEL: ", label)
-    return label
+    # print("LABEL: ", label)
+    return label, home, away
 
 def main():
 
@@ -44,23 +52,36 @@ def main():
     # s3.insert(1, "LABEL", "HOME")
 
     all_games = pd.concat([s1, s2, s3], ignore_index=True)
+    all_games.insert(0, "LABEL", "HOME")
+
+    for i, row in all_games.iterrows():
+        # if i == 1:
+            # print(row.get("SEASON_ID"))
+            l, home, away = label(row)
+            # print(l)
+            all_games.at[i, "LABEL"] = l
+            all_games.at[i, "HOME"] = home
+            all_games.at[i, "AWAY"] = away
+        
+
+    all_games = all_games[["GAME_ID", "SEASON_ID", "TEAM_ABBREVIATION", "MATCHUP", "WL", "HOME", "AWAY" ]]
     # for i in range(len(all_games)) :
     #     print(all_games.loc(i))
     # print(all_games)
     
 
-    all_games.insert(0, "LABEL", "HOME")
-    for i, row in all_games.iterrows():
-        # if i == 1:
-            print(row.get("SEASON_ID"))
-            l = label(row)
-            print(l)
-            all_games.at[i, "LABEL"] = l
-        
+    
+    # all_games.insert(5, "HOME", "HOME")
+    # all_games.insert(6, "AWAY", "HOME")
 
-    f = open("hist_game_logs.txt", "w")
+
+    all_games.drop_duplicates(subset=["GAME_ID"])
+
+    
+
+    f = open("logs.txt", "w")
     dfAsString = all_games.to_string()
-    l = dfAsString.splitlines()[1]
+    # l = dfAsString.splitlines()[1]
     # print(l)
 
 
