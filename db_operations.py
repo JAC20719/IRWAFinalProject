d8f74bf79609450bdea7f85c9c76ee2ea81259af
Justin Canedy <jcanedy07@gmail.com>
2022-05-13 15:32:27 -0400

    get teams working in db ops

--- db_operations.py
+++ db_operations.py
@@ -7,6 +7,7 @@ def get_team_stats(team_id, game_ids):
     t = tuple(game_ids)
     query = 'SELECT avg(CAST(PTS as INTEGER)) FROM gamelogs WHERE TEAM_ID = ? AND GAME_ID in {}'.format(t)
     c.execute(query, (team_id,)) 
+    #:)
 
 def main():
     get_team_stats("1610612741", ["0021500927","000000000"])
