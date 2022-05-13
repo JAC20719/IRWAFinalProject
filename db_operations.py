2fce7e8505978d8df16087f3756b56cf573fa1ff
isaacfrumkin <ifrumki1@jhu.edu>
2022-05-13 15:02:56 -0400

    trying avg

--- db_operations.py
+++ db_operations.py
@@ -1,10 +1,22 @@
 import sqlite3
-conn = sqlite3.connect('nba.db')
+conn = sqlite3.connect('example.db')
 c = conn.cursor()
 print("Database created and Successfully Connected to SQLite")
 
-c.execute("""CREATE TABLE team_stats (
-                team_id int,
-                
-    )""")
+def get_team_stats(team_id, game_ids):
+    bind = ','.join(i for i in game_ids)
+    print(bind)
+    c.execute('SELECT * FROM gamelogs WHERE TEAM_ID = ? GAME_ID in (%s)' %(bind,),(team_id,))  
 
+def main():
+    get_team_stats("1610612741", ("0021500927",))
+    print(c.fetchall())
+
+
+if __name__ == "__main__":
+    main()
+
+
+# ("0021500927", )
+#avg(CAST(PTS as INTEGER))
+#,
