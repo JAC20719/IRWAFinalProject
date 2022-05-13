cdad85c924d421bd12f88cab3395eb6f6bd3a8ea
Justin Canedy <jcanedy07@gmail.com>
2022-05-13 15:30:58 -0400

    get teams working in db ops

--- db_operations.py
+++ db_operations.py
@@ -4,19 +4,14 @@ c = conn.cursor()
 print("Database created and Successfully Connected to SQLite")
 
 def get_team_stats(team_id, game_ids):
-    bind = ','.join(i for i in game_ids)
-    print(bind)
-    c.execute('SELECT * FROM gamelogs WHERE TEAM_ID = ? GAME_ID in (%s)' %(bind,),(team_id,))  
+    t = tuple(game_ids)
+    query = 'SELECT avg(CAST(PTS as INTEGER)) FROM gamelogs WHERE TEAM_ID = ? AND GAME_ID in {}'.format(t)
+    c.execute(query, (team_id,)) 
 
 def main():
-    get_team_stats("1610612741", ("0021500927",))
+    get_team_stats("1610612741", ["0021500927","000000000"])
     print(c.fetchall())
 
 
 if __name__ == "__main__":
     main()
-
-
-# ("0021500927", )
-#avg(CAST(PTS as INTEGER))
-#,
