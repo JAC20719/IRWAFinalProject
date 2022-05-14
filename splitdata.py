8d214fe6be3be3ec8e34985db2fb3cb2246f2a15
isaacfrumkin <ifrumki1@jhu.edu>
2022-05-14 13:55:12 -0400

    random split

--- splitdata.py
+++ splitdata.py
@@ -1,8 +1,10 @@
 import sys
+import random
 
 file = sys.argv[1]
 with open(file) as f:
     lines = f.readlines()
+    random.shuffle(lines)
     lines = [l.strip() for l in lines]
     split = int(len(lines) * 0.9)
 with open(file[:-4] + '-train.txt', 'w') as f:
