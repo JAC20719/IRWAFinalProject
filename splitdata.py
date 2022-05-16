e3b085f2f4dd2c7a08a10f456eeed4c39d6a62af
isaacfrumkin <ifrumki1@jhu.edu>
2022-05-15 23:45:01 -0400

    training and dev w 2010-

--- splitdata.py
+++ splitdata.py
@@ -6,7 +6,7 @@ with open(file) as f:
     lines = f.readlines()
     random.shuffle(lines)
     lines = [l.strip() for l in lines]
-    split = int(len(lines) * 0.9)
+    split = int(len(lines) * 0.7)
 with open(file[:-4] + '-train.txt', 'w') as f:
     for line in lines[:split]:
         print(line, file=f)
